c312453ef02c4c19d0522bd6d349c6360c8a4634
Jackdream <821168115@qq.com>
2020-11-17 23:46:27 +0800

    添加Element-UI样式

--- utils-demo/src/main/java/com/example/utils/UtilsController.java
+++ utils-demo/src/main/java/com/example/utils/UtilsController.java
@@ -1,15 +1,28 @@
 package com.example.utils;
 
+import org.apache.commons.io.IOUtils;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 @RestController
 public class UtilsController {
 
     @RequestMapping("/regular")
-    public String regular(@RequestBody String data){
-        return data;
+    public List<String> regular(@RequestBody String data){
+        List<String> ls=new ArrayList<String>();
+        Pattern pattern = Pattern.compile("E.*\"");
+        Matcher matcher = pattern.matcher(data);
+        while(matcher.find())
+            ls.add(matcher.group() + "\n");
+        return ls;
     }
 }
